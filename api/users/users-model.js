493ee03c855ecec95729ac766a99150deec07909
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-08-04 20:53:04 -0700

    wip users-model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -10,25 +10,28 @@ function find() {
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
-
+  return db('users').where(filter);
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-
+  return db('users').where('user_id',user_id).first();
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
-function add(user) {
-
+async function add(user) {
+  await db('users').insert(user);
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
 
 module.exports = {
   find,
+  findBy,
+  findById,
+  add,
 };
