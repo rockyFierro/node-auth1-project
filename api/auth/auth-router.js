dbaa470f2bd23edd015b6b6cd586f8a188ce31d5
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-08-05 16:07:20 -0700

    :sparkles: passing mvp
    all test passing, all endpoints working

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -11,31 +11,40 @@ router.post('/register',
   checkPasswordLength, checkUsernameFree,
   (req, res, next) => {
     const { username, password } = req.body;
-    const hash = bcrypt.hashSync( password, 8); //2^10 
-    Users.add({username, password: hash})
-    .then(user => {
-      res.status(201).json(user);
-    })
-    .catch(next);
+    const hash = bcrypt.hashSync(password, 8); //2^10 
+    Users.add({ username, password: hash })
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(next);
   });
 
 router.post('/login',
   checkUsernameExists,
   (req, res, next) => {
     const { password } = req.body;
-    if (bcrypt.compareSync(password, req.user.password)){
+    if (bcrypt.compareSync(password, req.user.password)) {
       //set cookie to client
       //server stores session - user
       req.session.user = req.user;
-      res.json({ message: `Welcome ${req.user.username}!`});
-    }else{
-      next({ status: 401, message: 'Invalid credentials'});
+      res.json({ message: `Welcome ${req.user.username}!` });
+    } else {
+      next({ status: 401, message: 'Invalid credentials' });
     }
   });
 
 router.get('/logout',
   (req, res, next) => {
-    res.json('logout');
+    if (req.session.user) {
+      req.session.destroy(err => {
+        if (err) {
+          next(err);
+        } else {
+          res.json({ message: "logged out" });
+        } });
+    } else {
+      res.json({ message: "no session" });
+    }
   });
 
 
