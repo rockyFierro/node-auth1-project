50155d4e1759a3f4caf201dbf0699ecafadcbaa7
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-08-05 11:52:01 -0700

    :construction: restricted
    added functionality for protected pages

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -9,8 +9,11 @@ const Users = require('../users/users-model');
   }
 */
 function restricted(req, res, next) {
-  
-  next();
+  if(req.session.user){
+    next();
+  } else {
+    next({ status: 401, message: 'You shall not pass!'});
+  }
 }
 
 /*
